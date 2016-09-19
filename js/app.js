ac1cc727e2e5d9c7df3fc0cff2e7e02f6ed2d4ae
LizRuelas <liz.ruelas.borda@gmail.com>
2016-09-19 15:03:37 -0700

    subiendo cambios en el script

--- js/app.js
+++ js/app.js
@@ -1,14 +1,18 @@
 window.addEventListener("load", function() {
 	var boton=document.getElementById("button");
-	var textArea="";
 	boton.addEventListener("click",function(e){
 		e.preventDefault();
-		textArea= document.getElementById("textArea").value;
-   		var nuevoP = document.createElement("p");
+		var textArea= document.getElementById("textArea");
+   		mostrarMensaje(textArea.value);
+    	textArea.value="";
+	});
+
+	function mostrarMensaje(textArea){
+		var nuevoP = document.createElement("p");
     	var textNode = document.createTextNode(textArea);
     	nuevoP.appendChild(textNode);
     	var contenedor = document.getElementById("contenedor");
     	contenedor.insertBefore(nuevoP, contenedor.childNodes[0]);
-	});
+	}
 
 });
