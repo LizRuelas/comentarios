18434b81f2dc767ba768d543f9b6f56da92e21f0
LizRuelas <liz.ruelas.borda@gmail.com>
2016-09-20 10:30:44 -0700

    subiendo version 1 y 2

--- js/app.js
+++ js/app.js
@@ -1,19 +1,40 @@
 window.addEventListener("load", function() {
 	var boton=document.getElementById("button");
 	var textArea= document.getElementById("textArea");
+	var contador = document.getElementById("cont");
+	contador.innerHTML=140;
 	
-	boton.disabled=true;
-		
-
 	boton.addEventListener("click",function(e){
 		e.preventDefault();
-		
    		mostrarMensaje(textArea.value);
     	textArea.value="";
-
     	boton.disabled=true;
+    	contador.innerHTML=140;
+
 	});
 
+	textArea.addEventListener("keyup",function(){
+		boton.disabled=false;
+		var limite = 140;
+		var longitud = document.getElementById("textArea").value.length;
+		contador.innerText= limite-longitud;
+		if (limite <= longitud){
+			boton.disabled=true;
+		} else if (longitud==0){
+			boton.disabled=true;
+		}
+		if (longitud>=120  ){
+			contador.classList.add("colorBlue");
+		} else {
+			contador.classList.remove("colorBlue");
+		}
+		if (longitud>=130) {
+			contador.classList.remove("colorBlue");
+			contador.classList.add("colorRed");
+		} else {
+			contador.classList.remove("colorRed");
+		}
+	});
 	function mostrarMensaje(textArea){
 		var nuevoP = document.createElement("p");
     	var textNode = document.createTextNode(textArea);
@@ -21,23 +42,4 @@ window.addEventListener("load", function() {
     	var contenedor = document.getElementById("contenedor");
     	contenedor.insertBefore(nuevoP, contenedor.childNodes[0]);
 	}
-
-	textArea.addEventListener("keydown",function(){
-		boton.disabled=false;
-		var limite = 140;
-		var longitud = document.getElementById("textArea").value.length;
-		var contador = document.getElementById("cont");
-		contador.innerText= limite-longitud;
-		if (longitud>=limite){
-			contador.classList.add("colorRed");
-			boton.disabled=true;
-
-		} else {
-			contador.classList.remove("colorRed");
-		}
-	});
-	
-
-
-
 });
