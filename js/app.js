2c97bdce68057c81643e3a11d65c4822c405658f
LizRuelas <liz.ruelas.borda@gmail.com>
2016-09-21 11:17:50 -0800

    agregando la funcion bloquearboton en el js

--- js/app.js
+++ js/app.js
@@ -10,19 +10,27 @@ window.addEventListener("load", function() {
     	textArea.value="";
     	boton.disabled=true;
     	contador.innerHTML=140;
-
+    	textArea.style.cssText = 'height:50px'
+    	
 	});
 
 	textArea.addEventListener("keyup",function(){
 		boton.disabled=false;
+		cambioColor();
+		autosize();
+		bloquearBoton();
+
+	});
+
+	function autosize(){
+	    textArea.style.cssText = 'padding:0';
+	    textArea.style.cssText = 'height:' + textArea.scrollHeight + 'px';
+	}
+
+	function cambioColor(){
 		var limite = 140;
-		var longitud = document.getElementById("textArea").value.length;
+		var longitud = textArea.value.length;
 		contador.innerText= limite-longitud;
-		if (limite <= longitud){
-			boton.disabled=true;
-		} else if (longitud==0){
-			boton.disabled=true;
-		}
 		if (longitud>=120  ){
 			contador.classList.add("colorBlue");
 		} else {
@@ -34,12 +42,39 @@ window.addEventListener("load", function() {
 		} else {
 			contador.classList.remove("colorRed");
 		}
-	});
+	}
+
+	function bloquearBoton(){
+		var limite = 140;
+		var longitud = textArea.value.length;
+		var textAreaVacio = textArea.value.replace(/\s/g,"");
+		if (limite <= longitud || longitud==0 || textAreaVacio == 0){
+			boton.disabled=true;
+		} 
+	}
+	
+
 	function mostrarMensaje(textArea){
 		var nuevoP = document.createElement("p");
     	var textNode = document.createTextNode(textArea);
     	nuevoP.appendChild(textNode);
     	var contenedor = document.getElementById("contenedor");
     	contenedor.insertBefore(nuevoP, contenedor.childNodes[0]);
+    	reloj(nuevoP);
 	}
+
+	function reloj(re){
+		var momentoActual = new Date() ;
+	   	var hora = momentoActual.getHours() ;
+	   	var minuto = momentoActual.getMinutes() ;
+	   		if (minuto<10) {
+	   			minuto ="0" + minuto;
+	   		}
+	   	var horaImprimible = " "+ hora + " : " + minuto  ;
+	   	var hora = document.createElement("span");
+	   	hora.innerText=horaImprimible ;
+		re.insertBefore(hora,re.childNodes[1]);
+		hora.classList.add("izquierda");
+	}
+	
 });
